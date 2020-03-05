718a0830ea80c6a81ca61d077e6b0b8907669232
izsie <izsie.robinson@gmail.com>
2020-03-05 11:00:28 -0600

    updated models

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,19 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @Size(max = 3000, message = "Description should be less than 3000 characters.")
+    private String skillDescription;
+
+    public Skill() {}
+
+    public String getSkillDescription() { return skillDescription; }
+
+    public void setSkillDescription(String skillDescription) {
+        this.skillDescription = skillDescription;
+    }
 }
